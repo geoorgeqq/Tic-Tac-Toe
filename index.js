20accc2a9b731234122c186fa265f56df5007ac5
geoorgeqq <grajdeanu.george@yahoo.ro>
2023-11-03 02:39:38 +0200

    JS Add X and 0

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let squares = document.getElementsByClassName('square');
 let container = document.querySelector(".container");
-
-
-let selector = "x";
-
-
+let lastClicked;
 for (let square of squares){
-    square.addEventListener("click", () =>{
-        square.textContent = selector;
-    })
-}
+    square.addEventListener("click", () =>{    
+        let selector = "X";
+        if(selector !==lastClicked && square.textContent ==""){
+            square.textContent = "X";
+            lastClicked = "X";
+        }else if(square.textContent ==""){
+            square.textContent ="O";
+            lastClicked = "O";
+        }
+        
+        })
+    }
+
+
+
 
 
